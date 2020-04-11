3dc902f78be00d3e2c0d4b90bb813d6a3edb51e4
Shashwat Singh <shashanoid@gmail.com>
2020-04-10 20:04:45 -0400

    test deploy

---
+++ proj/precache-manifest.ab39ba45ef2ad5647b39111794f51ae8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dc654ee57b2465a623fae59a3823824",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6eb2b886bdd8b8d85a5c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "744735684d3b8d965334",
+    "url": "/static/js/2.d861d29a.chunk.js"
+  },
+  {
+    "revision": "23900771a863482f56e68d68cf1f3072",
+    "url": "/static/js/2.d861d29a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6eb2b886bdd8b8d85a5c",
+    "url": "/static/js/main.f0a167b1.chunk.js"
+  },
+  {
+    "revision": "f65428246bf6eb5b06dc",
+    "url": "/static/js/runtime-main.55fb12cf.js"
+  }
+]);
